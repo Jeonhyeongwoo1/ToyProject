d8b270848c5656dfa5499c126a18dd52fe8ada2f
Jeonhyeongwoo1 <guddn1234k@gmail.com>
2023-01-27 01:08:43 +0900

    Update roulette

--- Assets/Roulette/Roulette.cs
+++ Assets/Roulette/Roulette.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using System;
+using DG.Tweening;
 
 namespace Roulette
 {
@@ -9,14 +12,29 @@ namespace Roulette
     {
         [SerializeField] private ScrollRect _ScrollRect;
 
+        [SerializeField] private bool _IsVertical;
         [SerializeField] private float _Threshold = 100;
         [SerializeField] private float _DisableMargin;
-        [SerializeField] private float _OffsetY;
+        [SerializeField] private float _Offset;
+        [SerializeField] private int _CardCount;
         [SerializeField] private List<RectTransform> _ItemList = new List<RectTransform>();
+        [SerializeField] private List<RollingData> _RollingData = new List<RollingData>();
 
-        /*
-            1. 
-        */
+        public enum DirectionType
+        {
+            LEFT,
+            RIGHT
+        }
+
+        [Serializable]
+        public struct RollingData
+        {
+            public DirectionType directionType;
+            public int countCount;
+            public float duration;
+            public float delay;
+            public AnimationCurve animationCurve;
+        }
 
         private void Start()
         {
@@ -29,48 +47,108 @@ namespace Roulette
                 _ItemList.Add(rect);
             }
 
-            float axisY = _ItemList[0].anchoredPosition.y - _ItemList[1].anchoredPosition.y;
-            if (axisY < 0)
+            if (_IsVertical)
             {
-                axisY *= -1;
+                float axisY = _ItemList[0].anchoredPosition.y - _ItemList[1].anchoredPosition.y;
+                if (axisY < 0)
+                {
+                    axisY *= -1;
+                }
+
+                _Offset = axisY;
+                _DisableMargin = _Offset * childCount / 2;
+            }
+            else
+            {
+                float axisX = _ItemList[0].anchoredPosition.x - _ItemList[1].anchoredPosition.x;
+                if (axisX < 0)
+                {
+                    axisX *= -1;
+                }
+
+                _Offset = axisX;
+                _DisableMargin = _Offset * childCount / 2;
+
+
             }
 
-            _OffsetY = axisY;
-            _DisableMargin = _OffsetY * childCount / 2;
+            _ScrollRect.content.sizeDelta = _ItemList[0].sizeDelta;
 
         }
 
-        private Vector2 itemPos;
+        [Button]
+        private void MoveTo()
+        {
+            StartCoroutine(MoveCor());
+        }
+
+        private IEnumerator MoveCor()
+        {
+            for (int i = 0; i < _RollingData.Count; i++)
+            {
+                bool isComplete = false;
+                RollingData data = _RollingData[i];
+                int dir = data.directionType == DirectionType.RIGHT ? 1 : -1;
+                float movePosX = _ScrollRect.content.anchoredPosition.x + data.countCount * _Offset * dir;
+                _ScrollRect.content.DOAnchorPosX(movePosX, data.duration)
+                                    .SetEase(data.animationCurve)
+                                    .OnComplete(() => isComplete = true);
+
+                while (!isComplete)
+                {
+                    yield return null;
+                }
+
+                yield return new WaitForSeconds(data.delay);
+            }
+        }
+
         private void OnScroll(Vector2 value)
         {
             for (int i = 0; i < _ItemList.Count; i++)
             {
-                RectTransform item = _ItemList[i];
-                //맨 밑의 경우 child 0으로 변경한다
-                if (i == 0)
+                if (_IsVertical)
                 {
-                    Debug.Log(_ScrollRect.transform.InverseTransformPoint(item.gameObject.transform.position).y);
-                }
+                    RectTransform item = _ItemList[i];
+                    float posY = _ScrollRect.transform.InverseTransformPoint(item.position).y;
+                    if (posY < -_DisableMargin)
+                    {
+                        float newPosY = item.anchoredPosition.y;
+                        newPosY += _Offset * _ItemList.Count;
+                        item.anchoredPosition = new Vector2(item.anchoredPosition.x, newPosY);
+                        _ScrollRect.content.GetChild(0).transform.SetAsLastSibling();
+                    }
 
-                float posY = _ScrollRect.transform.InverseTransformPoint(item.position).y;
-                if (posY < -_DisableMargin)
-                {
-                    float newPosY = item.anchoredPosition.y;
-                    newPosY += _OffsetY * _ItemList.Count;
-                    item.anchoredPosition = new Vector2(item.anchoredPosition.x, newPosY);
-                    _ScrollRect.content.GetChild(0).transform.SetAsLastSibling();
+                    if (posY > _DisableMargin + _Threshold)
+                    {
+                        float newPosY = item.anchoredPosition.y;
+                        newPosY -= _Offset * _ItemList.Count;
+                        item.anchoredPosition = new Vector2(item.anchoredPosition.x, newPosY);
+                        _ScrollRect.content.GetChild(_ItemList.Count - 1).SetAsFirstSibling();
+                    }
                 }
-
-                if (posY > _DisableMargin + _Threshold)
+                else
                 {
-                    float newPosY = item.anchoredPosition.y;
-                    newPosY -= _OffsetY * _ItemList.Count;
-                    item.anchoredPosition = new Vector2(item.anchoredPosition.x, newPosY);
-                    _ScrollRect.content.GetChild(_ItemList.Count - 1).SetAsFirstSibling();
+                    RectTransform item = _ItemList[i];
+                    float posX = _ScrollRect.transform.InverseTransformPoint(item.position).x;
+                    if (posX < -_DisableMargin)
+                    {
+                        float newPosX = item.anchoredPosition.x;
+                        newPosX += _Offset * _ItemList.Count;
+                        item.anchoredPosition = new Vector2(newPosX, item.anchoredPosition.y);
+                        _ScrollRect.content.GetChild(0).transform.SetAsLastSibling();
+                    }
+
+                    if (posX > _DisableMargin + _Threshold)
+                    {
+                        float newPosX = item.anchoredPosition.x;
+                        newPosX -= _Offset * _ItemList.Count;
+                        item.anchoredPosition = new Vector2(newPosX, item.anchoredPosition.y);
+                        _ScrollRect.content.GetChild(_ItemList.Count - 1).SetAsFirstSibling();
+                    }
                 }
 
             }
         }
-
     }
 }
